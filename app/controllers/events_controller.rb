6a8d6168a3f337587883a8c07e42398201f16bc6
KJ Roberts <kjroberts39@hotmail.com>
2017-04-28 16:08:40 -0600

    added creatability for events needs testing with forms

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,18 +15,38 @@ class EventsController < ApplicationController
   end
 
   def create
+    @event = Event.new(event_params)
 
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to @event, notice: 'Your event is now online.' }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   def edit
   end
 
   def update
-
+    resond_to do |format|
+     
+      if @event.update(event_params)
+        format.html { redirect_to @event, notice: 'Your information was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
   end
 
   def destroy
 
+    @event.destroy
+    respond_to do |format|
+      format.html { redirect_to homepage_url, notice: 'Event was removed.' }
+      format.json { head :no_content }
+    end
   end
 
 
@@ -35,5 +55,9 @@ class EventsController < ApplicationController
 	def set_event
 		@event = Event.find(params[:id])
 	end
+
+  def event_params
+    params.require(:event).permit(:title, :summary, :location, :event_date, :approved_ids, :max_attendees)
+  end
 
 end
