458a95a381eb5a3f4178cff3384a11af399ad539
KJ Roberts <kjroberts39@hotmail.com>
2017-04-28 13:12:40 -0600

    Basic event creation ability

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,37 @@
 class EventsController < ApplicationController
+	before_action :set_event, only: [:show, :destroy, :edit, :update]
   def index
+  	@events = Event.all
+  end
+
+  def show
+  	
+  end
+
+  def new
+  	@event = Event.new
+  end
+
+  def create
 
   end
+
+  def edit
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+
+	private
+
+	def set_event
+		@event = Event.find(params[:id])
+	end
+
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,9 @@
 class Event < ApplicationRecord
+
+	belongs_to :user
+	has_many :attendees, dependent: :destroy
+
+validates_presence_of :title, :summary, :location, :event_date, :approved_ids 
+
+
 end
